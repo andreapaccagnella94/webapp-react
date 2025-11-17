7e462c57e849c0f2059a9b084b38fefd7f1fd248
andreapaccagnella94 <andreapacca94@live.it>
2025-11-18 00:08:31 +0100

    Implement review submission functionality in MoviePage component

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -8,16 +8,26 @@ export default function MoviePage() {
 
     const { id } = useParams();
     const API_URL = `http://localhost:3000/api/movies/${id}`
-
+    // useState per caricare il film con la chiamata axios.get
     const [movie, setMovie] = useState(null)
     // predisponiamo una costatnte per prendere l'errore per un fututo Bonus
     const [error, setError] = useState(null);
+    // variabili per informazioni iniziali form + useState per passare i dati che inserisco
+    const initialForm = {
+        name: "",
+        vote: 0,
+        text: ""
+    }
+    const [form, setForm] = useState(initialForm)
+    // useState per il numero delle recensioni così da ricaricare la pagina
+    const [reviewNumber, setReviewNumber] = useState(0)
 
     useEffect(() => {
         axios.get(API_URL)
             .then(res => {
                 console.log(res.data);
                 setMovie(res.data)
+
             })
             .catch(err => {
                 // TODO: it would be great to show a user-friendly message in the UI (maybe use a useState hook to store the error and show it)
@@ -25,7 +35,27 @@ export default function MoviePage() {
                 console.log(err.message);
                 setError(err.message);
             })
-    }, [])
+    }, [reviewNumber])
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log(form);
+        axios.post(`${API_URL}/reviews`, form, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                console.log(res);
+                setForm(initialForm)
+                setReviewNumber(movie.reviews.length)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+
+    }
 
     return (
 
@@ -53,18 +83,18 @@ export default function MoviePage() {
             <div className="mb-4">
                 <div className="container">
                     <h3>Leave a Review</h3>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-3">
                             <label htmlFor="name" className="form-label">Name</label>
-                            <input name="name" type="text" className="form-control" id="name" placeholder="anonymous" />
+                            <input name="name" type="text" className="form-control" id="name" placeholder="type your name" value={form.name} onChange={(e) => setForm({ ...form, name: e.target.value })} />
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="review" className="form-label">Review</label>
-                            <textarea name="review" className="form-control" id="review" rows="3" placeholder="Write your review here..."></textarea>
+                            <label htmlFor="text" className="form-label">Review</label>
+                            <textarea name="text" className="form-control" id="text" rows="3" placeholder="Write your review here..." value={form.text} onChange={(e) => setForm({ ...form, text: e.target.value })}></textarea>
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="rating" className="form-label">Rating</label>
-                            <select name="rating" className="form-select" id="rating">
+                            <label htmlFor="vote" className="form-label">Vote rating</label>
+                            <select name="vote" className="form-select" id="vote" value={form.vote} onChange={(e) => setForm({ ...form, vote: e.target.value })}>
                                 <option value="1">1 Star</option>
                                 <option value="2">2 Stars</option>
                                 <option value="3">3 Stars</option>
